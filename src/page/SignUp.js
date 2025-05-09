f66f8c551574c23bc59759df7a57075806765fcc
Omar3yyad25 <202000806@zewailcity.edu.eg>
2025-05-09 18:33:36 +0300

    edits

---
+++ src/page/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CommonBanner from '../component/Common/Banner'
+import SignUpForm from "../component/SignUp";
+import OurPartner from '../component/Common/OurPartner'
+
+const SignUp = () => {
+    return (
+        <>
+            <CommonBanner heading="SignUp" page="SignUp"/>
+            <SignUpForm heading="Create an Account!"/>
+        </>
+    )
+}
+
+export default SignUp
